ff97e93cc873af449518acb256f7bb198bfcb78e
tr4m0ryp - AppelMoussa <tramoryp@proton.me>
2025-01-25 11:51:53 +0100

    Update valid_country_iban.js

--- Iban_verification/valid_country_iban.js
+++ Iban_verification/valid_country_iban.js
@@ -1,217 +1,91 @@
 
 
 function valid_county_iban(iban){
+
+    const country_IB2 = {
+        AL: [28],
+        AD: [24],
+        AT: [20],
+        AZ: [28],
+        BH: [22],
+        BE: [16],
+        BA: [20],
+        BG: [22],
+        HR: [21],
+        CY: [28],
+        CZ: [24],
+        DK: [18],
+        EE: [20],
+        FO: [18],
+        FI: [18],
+        FR: [27],
+        DE: [22],
+        GI: [23],
+        GR: [27],
+        GL: [18],
+        HU: [28],
+        IS: [26],
+        IE: [22],
+        IL: [23],
+        IT: [27],
+        JO: [30],
+        KZ: [20],
+        XK: [20],
+        KW: [30],
+        LV: [21],
+        LB: [28],
+        LI: [21],
+        LT: [20],
+        LU: [20],
+        MK: [19],
+        MT: [31],
+        MR: [27],
+        MU: [30],
+        MD: [24],
+        MC: [27],
+        ME: [22],
+        NL: [18],
+        NO: [15],
+        PK: [24],
+        PS: [29],
+        PL: [28],
+        PT: [25],
+        QA: [29],
+        RO: [24],
+        SM: [27],
+        SA: [24],
+        RS: [22],
+        SC: [31],
+        SK: [24],
+        SI: [19],
+        ES: [23],
+        SE: [24],
+        CH: [21],
+        TN: [24],
+        TR: [26],
+        AE: [23],
+        GB: [22],
+        VA: [22],
+    };
+
+    const landcode = iban.slice(0,2)
+
+    if (country_IB2.hasOwnProperty(landcode)){
+        
+        const expected_length = country_IB2[landcode][0];
+       
+        if (expected_length == iban.length){
+            return true;
+        }else{
+            console.log('non-valid iban');
+            return false;
+        };
     
-    //ALbanie
-    if (iban.slice(0, 2) == "AL") {
-        if (iban.length != 28) {
-            return false;
-        }else {
-            return iban
-        }; 
-    };
+    }else{
+    console.log("Landcode", landcode, " Isn't a valid")
+    return false;
+};
 
-    //Andorra
-    if (iban.slice(0,2) == "AD") {
-        if (iban.length != 24) {
-            return false;
-        }else {
-            return iban;
-        };
-    };
-
-    //Oostenrijk
-    if (iban.slice(0,2) == "AT") {
-        if (iban.length != 20) {
-            return false;
-        }else {
-            return iban;
-        };
-    };
-
-    //Azerbeidzjan 
-    if (iban.slice(0,2) == "AZ") {
-        if (iban.length != 28) {
-            return false;
-        }else{
-            return iban
-        };
-    };
-
-    //Bahrein 
-    if (iban.slice(0,2) == "BH") {
-        if (iban.length != 22) {
-            return false;
-        }else {
-            return iban;
-        };
-    };
-
-
-    //België 
-    if (iban.slice(0,2) == "BE") {
-        if (iban.length != 16){
-            return false;
-        }else{
-            return iban;
-        };
-    };
-
-    //Bosnië-Herzegovina
-    if (iban.slice(0,2) == "BA"){
-        if (iban.length != 20){
-            return false;
-        }else{
-            return iban;
-        };
-    };
-
-    //Bulgarije 
-    if (iban.slice(0,2) == "BG"){
-        if (iban.length != 22){
-            return false;
-        }else{
-            return iban;
-        };
-    };
-    
-    //Kroatie
-    if (iban.slice(0,2) == "HR"){
-        if (iban.length != 21){
-            return false;
-        }else{
-            return iban;
-        };
-    };
-
-
-    //Cyprus
-    if (iban.slice(0,2) == "CY"){
-        if (iban.length != 28){
-            return false;
-        }else{
-            return iban;
-        };
-    };
-
-
-    //Tsjechie
-    if (iban.slice(0, 2) == "CZ"){
-        if (iban.length != 24){
-            return false;   
-        }else{
-            return iban;
-        };
-    };
-
-
-    //Denemarken
-    if (iban.slice(0, 2) == "DK"){
-        if (iban.length != 18){
-            return false;
-        }else{
-            return iban;
-        };
-    };
-
-    //Estland
-    if (iban.slice(0, 2) == "EE"){
-        if (iban.length != 20){
-            return false; 
-        }else{
-            return iban;
-        };
-    };
-
-
-    //Faeroer
-    if (iban.slice(0, 2) == "FO"){
-        if (iban.length != 18){
-            return false;
-        }else{
-            return iban;
-        };
-    };
-
-
-    //Frankrijk
-    if (iban.slice(0, 2) == "FR"){
-        if (iban.length != 27){
-            return false;
-        }else{
-            return iban;
-        };
-    };
-
-    //Duitsland
-    if (iban.slice(0, 2) == "DE"){
-        if (iban.length != 22){
-            return false;
-        }else{
-            return iban;
-        };
-    };
-
-
-    //Gibralar
-    if (iban.slice(0, 2) == "GI"){
-        if (iban.length != 23){
-            return false;
-        }else{
-            return iban;
-        };
-    };
-
-
-    //Griekenland
-    if (iban.slice(0,2) == "GR"){
-        if (iban.length != 27){
-            return false;
-        }else{
-            return iban;
-        };
-    };
-
-
-    //Groenland
-    
-    //Hongarije
-    //Ijsland
-    //Ierland
-    //Israel
-    //Italie
-    //Jordanie
-    //Kazachstan
-    //Kosovo
-    //Koeweit
-    //Letland
-    //Libanon
-    //Liechtenstein
-    //Litouwen
-    //Luxenburg
-    //Noord-Macedonie
-    //Malta
-    //Mauritanie
-    //Mauritius
-    //Moldavie
-    //Monaco
-    //Montenegro 
-    //Nederland
-    //Noorwegen
-    //Pakistan
-    //palestina
-    //polen
-    //portugal
-    //quatar
-    //roemenie
-    //san marino
-    //saoudie-arivie
-    //servie
-    //seychellen
-    //slowakije
-    //slovenie
-    //spanje
-    //zweden
-    //zwitserland
 
 }
+
