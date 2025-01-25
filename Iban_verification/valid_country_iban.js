28776d8ff6e4b7671858d4ac74c7d237e1f39216
tr4m0ryp - AppelMoussa <tramoryp@proton.me>
2025-01-25 02:05:56 +0100

    Create valid_country_iban.js

---
+++ Iban_verification/valid_country_iban.js
@@ -0,0 +1,217 @@
+
+
+function valid_county_iban(iban){
+    
+    //ALbanie
+    if (iban.slice(0, 2) == "AL") {
+        if (iban.length != 28) {
+            return false;
+        }else {
+            return iban
+        }; 
+    };
+
+    //Andorra
+    if (iban.slice(0,2) == "AD") {
+        if (iban.length != 24) {
+            return false;
+        }else {
+            return iban;
+        };
+    };
+
+    //Oostenrijk
+    if (iban.slice(0,2) == "AT") {
+        if (iban.length != 20) {
+            return false;
+        }else {
+            return iban;
+        };
+    };
+
+    //Azerbeidzjan 
+    if (iban.slice(0,2) == "AZ") {
+        if (iban.length != 28) {
+            return false;
+        }else{
+            return iban
+        };
+    };
+
+    //Bahrein 
+    if (iban.slice(0,2) == "BH") {
+        if (iban.length != 22) {
+            return false;
+        }else {
+            return iban;
+        };
+    };
+
+
+    //België 
+    if (iban.slice(0,2) == "BE") {
+        if (iban.length != 16){
+            return false;
+        }else{
+            return iban;
+        };
+    };
+
+    //Bosnië-Herzegovina
+    if (iban.slice(0,2) == "BA"){
+        if (iban.length != 20){
+            return false;
+        }else{
+            return iban;
+        };
+    };
+
+    //Bulgarije 
+    if (iban.slice(0,2) == "BG"){
+        if (iban.length != 22){
+            return false;
+        }else{
+            return iban;
+        };
+    };
+    
+    //Kroatie
+    if (iban.slice(0,2) == "HR"){
+        if (iban.length != 21){
+            return false;
+        }else{
+            return iban;
+        };
+    };
+
+
+    //Cyprus
+    if (iban.slice(0,2) == "CY"){
+        if (iban.length != 28){
+            return false;
+        }else{
+            return iban;
+        };
+    };
+
+
+    //Tsjechie
+    if (iban.slice(0, 2) == "CZ"){
+        if (iban.length != 24){
+            return false;   
+        }else{
+            return iban;
+        };
+    };
+
+
+    //Denemarken
+    if (iban.slice(0, 2) == "DK"){
+        if (iban.length != 18){
+            return false;
+        }else{
+            return iban;
+        };
+    };
+
+    //Estland
+    if (iban.slice(0, 2) == "EE"){
+        if (iban.length != 20){
+            return false; 
+        }else{
+            return iban;
+        };
+    };
+
+
+    //Faeroer
+    if (iban.slice(0, 2) == "FO"){
+        if (iban.length != 18){
+            return false;
+        }else{
+            return iban;
+        };
+    };
+
+
+    //Frankrijk
+    if (iban.slice(0, 2) == "FR"){
+        if (iban.length != 27){
+            return false;
+        }else{
+            return iban;
+        };
+    };
+
+    //Duitsland
+    if (iban.slice(0, 2) == "DE"){
+        if (iban.length != 22){
+            return false;
+        }else{
+            return iban;
+        };
+    };
+
+
+    //Gibralar
+    if (iban.slice(0, 2) == "GI"){
+        if (iban.length != 23){
+            return false;
+        }else{
+            return iban;
+        };
+    };
+
+
+    //Griekenland
+    if (iban.slice(0,2) == "GR"){
+        if (iban.length != 27){
+            return false;
+        }else{
+            return iban;
+        };
+    };
+
+
+    //Groenland
+    
+    //Hongarije
+    //Ijsland
+    //Ierland
+    //Israel
+    //Italie
+    //Jordanie
+    //Kazachstan
+    //Kosovo
+    //Koeweit
+    //Letland
+    //Libanon
+    //Liechtenstein
+    //Litouwen
+    //Luxenburg
+    //Noord-Macedonie
+    //Malta
+    //Mauritanie
+    //Mauritius
+    //Moldavie
+    //Monaco
+    //Montenegro 
+    //Nederland
+    //Noorwegen
+    //Pakistan
+    //palestina
+    //polen
+    //portugal
+    //quatar
+    //roemenie
+    //san marino
+    //saoudie-arivie
+    //servie
+    //seychellen
+    //slowakije
+    //slovenie
+    //spanje
+    //zweden
+    //zwitserland
+
+}
